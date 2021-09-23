cb35f838e8879cc2be6369d2e504074187ed2e7e
mherrera01 <miguelherrera8600@gmail.com>
2021-09-23 19:57:44 +0200

    Calculate mcd function added

--- Práctica 1/src/main.c
+++ Práctica 1/src/main.c
@@ -2,25 +2,34 @@
 #include <stdlib.h>
 #include <gmp.h>
 
+mpz_t *calculate_mcd(mpz_t *r, mpz_t *a, mpz_t *b){
+    if(mpz_cmp_si(*a, 0) == 0) return a;
+    if(mpz_cmp_si(*a, 1) == 0) return r;
+    mpz_mod(*r, *a, *b);
+    return calculate_mcd(a, b, r);
+
+    //mpz_mod (mpz_t r, const mpz_t n, const mpz_t d)
+    //mpz_cmp_si (const mpz_t op1, signed long int op2)
+    //mpz_cmp (const mpz_t op1, const mpz_t op2)
+}
+
 int main (int argc,char *argv[]) {
-    mpz_t a, b, m;
-    int modo;
-    FILE *entrada = NULL, *salida = NULL;
+    mpz_t a,b,r;
+    char sa[100], sb[100];
 
-    mpz_init (a);
-    mpz_init (b);
-    mpz_init (m);
+    mpz_init(a);
+    mpz_init(b);
+    mpz_init(r);
+    mpz_set_str(r,"1",10);
 
-    mpz_set_str (a,"123452345234523452352352345112341234213",10);
-    mpz_set_str (b,"234562344341234123421341234441234213421",10);
+    printf("Introduce un valor para a:\n");
+    scanf("%s", sa);
+    mpz_set_str (a,sa,10);
+    printf("Introduce un valor para b:\n");
+    scanf("%s", sb);
+    mpz_set_str (b,sb,10);
 
-    mpz_add    (m,a,b);
+    gmp_printf("El mcd de %Zd y %Zd es %Zd\n", a, b, calculate_mcd(&r, &a, &b));
 
-    gmp_printf ("El resultado de la suma es %Zd\n", m);
-
-    mpz_clear (a);
-    mpz_clear (b);
-    mpz_clear (m);
-
-    return(0);
+    return 0;
 }
