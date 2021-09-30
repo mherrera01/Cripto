acf2c120f23c8cd088d33e0a5312306bb394dce0
mherrera01 <miguelherrera8600@gmail.com>
2021-09-30 19:39:12 +0200

    Functionality working, fixing Valgrind
    Co-authored-by: Jose Manuel Freire Porras <jose.freire@estudiante.uam.es>

--- Práctica 1/src/main.c
+++ Práctica 1/src/main.c
@@ -14,41 +14,26 @@ mpz_t *calculate_mcd(mpz_t *r, mpz_t *a, mpz_t *b){
     //mpz_cmp (const mpz_t op1, const mpz_t op2)
 }
 
-/*
-get_inverse(n, m){
-    ma = m
-    na = n
-    for(i = 0; x!=0; i++){
-        x=ma%na
-        c[2]=c[1]
-        c[1]=c[0]
-        c[0]=ma/na
-        p[i] = (p[i-2] - p[i-1]*c[2])%m
-    }
-    if(na!=1) fail;
-    p[i+1] = (p[i-1] - p[i]*c[1])%m
-}
-*/
-
 void get_modular_inverse(mpz_t *r, mpz_t *m, mpz_t *n){
     mpz_t ma, na, x, c[3];
     mpz_t *p = NULL;
-    int i;
+    int i, p_size;
 
     mpz_init(ma);
     mpz_init(na);
-    mpz_init(c[0]);
-    mpz_init(c[1]);
-    mpz_init(c[2]);
+
+    for (i=0; i<3; i++) mpz_init(c[i]);
     mpz_init(x);
-    p = (mpz_t*) malloc(200*sizeof(mpz_t));
+
+    p_size = 2;
+    p = (mpz_t*) malloc(p_size*sizeof(mpz_t));
 
     mpz_set (ma, *m);
     mpz_set (na, *n);
 
     mpz_set_str(x, "1", 10);
 
-    for(i=0; i<200; i++){
+    for(i=0; i<p_size; i++){
         mpz_init(p[i]);
     }
 
@@ -59,13 +44,17 @@ void get_modular_inverse(mpz_t *r, mpz_t *m, mpz_t *n){
         mpz_set(c[2], c[1]);
         mpz_set(c[1], c[0]);
         mpz_fdiv_q (c[0], ma, na);
-        gmp_printf("%Zd = %Zd(%Zd) + %Zd    ", ma, c[0], na, x);
+        gmp_printf("%Zd = %Zd(%Zd) + %Zd    \t", ma, c[0], na, x);
         mpz_set(ma, na);
         if(mpz_cmp_si(x, 0) != 0) mpz_set(na, x);
-        mpz_mul(p[i], p[i-1], c[2]);
-        mpz_sub(p[i], p[i-2], p[i]);
-        mpz_mod(p[i], p[i], *m);
-        gmp_printf("p=%Zd\n", p[i]);
+        if(i>1){
+            mpz_mul(p[i], p[i-1], c[2]);
+            mpz_sub(p[i], p[i-2], p[i]);
+            mpz_mod(p[i], p[i], *m);
+        }
+        p = realloc(p, ++p_size);
+        mpz_init(p[p_size-1]);
+        //gmp_printf("p = %Zd - %Zd(%Zd) mod %Zd = %Zd p_size=%d\n",p[i-2], p[i-1], c[2], m, p[i], p_size);
     }
 
     if(mpz_cmp_si(na, 1) != 0){
@@ -74,30 +63,44 @@ void get_modular_inverse(mpz_t *r, mpz_t *m, mpz_t *n){
         return;
     }
     //p[i+1] = (p[i-1] - p[i]*c[1])%m
-    mpz_mul(p[i+1], p[i], c[1]);
-    mpz_sub(p[i+1], p[i-1], p[i+1]);
-    mpz_mod(p[i+1], p[i+1], *m);
+    mpz_mul(p[i], p[i-1], c[1]);
+    mpz_sub(p[i], p[i-2], p[i]);
+    mpz_mod(p[i], p[i], *m);
 
-    gmp_printf("p[i+1] es %Zd\n", p[i+1]);
-    mpz_set(*r, p[i+1]);
+    gmp_printf("p = %Zd - %Zd(%Zd) mod %Zd = %Zd\n",p[i-2], p[i-1], c[1], m, p[i]);
+    mpz_set(*r, p[i]);
+
+    mpz_clear(ma);
+    mpz_clear(na);
+    mpz_clear(x);
+    for(i=0; i<2; i++) mpz_clear(c[i]);
+    for (i=0; i<p_size; i++) mpz_clear(p[i]);
+    free(p);
+
     return;
 }
 
 int main (int argc,char *argv[]) {
-    mpz_t a,b,r;
+    mpz_t a, b, r;
     char sa[100], sb[100];
 
     mpz_init(a);
     mpz_init(b);
     mpz_init(r);
-    mpz_set_str(r,"1",10);
+    mpz_set_str(r, "1", 10);
 
+    
     printf("Introduce un valor para a:\n");
     scanf("%s", sa);
     mpz_set_str (a,sa,10);
     printf("Introduce un valor para b:\n");
     scanf("%s", sb);
     mpz_set_str (b,sb,10);
+    
+    /*
+    mpz_set_str (a,"26",10);
+    mpz_set_str (b,"15",10);
+    */
 
     get_modular_inverse(&r, &a, &b);
 
