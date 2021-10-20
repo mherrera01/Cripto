21e758f67e260286d8e175d7258b4ed66e0ed2a5
mherrera01 <miguelherrera8600@gmail.com>
2021-10-20 13:42:40 +0200

    Lectura de bloques en vigenere implementado

--- Práctica 1/src/vigenere.c
+++ Práctica 1/src/vigenere.c
@@ -3,11 +3,17 @@
 #include <string.h>
 #include "../includes/alphabet.h"
 
-int vigenere_encrypt() {
+int vigenere_encrypt(char *message, char *key, int m) {
+    // Control de errores
+    if (message == NULL || key == NULL || m <= 0) return -1;
+
     return 0;
 }
 
-int vigenere_decrypt() {
+int vigenere_decrypt(char *message, char *key, int m) {
+    // Control de errores
+    if (message == NULL || key == NULL || m <= 0) return -1;
+
     return 0;
 }
 
@@ -35,8 +41,9 @@ void close_files (FILE *input, FILE *output) {
 }
 
 int main (int argc, char *argv[]) {
-    char *k, *convertToLong, message[100]; // TODO
-    int i, keyLength = 0, m = 26, modo = 0; // modo en 0 para cifrar y 1 para descifrar
+    char *k, *convertToLong, *message = NULL;
+    int modo = 0; // modo en 0 para cifrar y 1 para descifrar
+    int i, endRead = 0, keyLength = 0, m = 26;
     FILE *input = stdin, *output = stdout;
 
     // Comprobar número de argumentos del usuario
@@ -47,7 +54,7 @@ int main (int argc, char *argv[]) {
     }
 
     // Obtener parámetros del usuario
-    // vigenere {-C|-D} {-k clave} [-i filein] [-o fileout]
+    // vigenere {-C|-D} {-k clave} [-m |Zm|] [-i filein] [-o fileout]
     for (i = 1; i < argc; i++) {
         if (strcmp("-C", argv[i]) == 0) {
             // Cifrar
@@ -122,39 +129,61 @@ int main (int argc, char *argv[]) {
         return -1;
     }
 
-    while (fgets(message, keyLength, input) != NULL) {
-        printf("%s\n", message);
+    // Inicializamos el buffer del mensaje a leer con la longitud de la clave
+    message = (char *) malloc ((keyLength + 1) * sizeof(char));
+    if (message == NULL) {
+        printf("Error: No se ha podido inicializar la memoria del mensaje a leer.\n");
+
+        destroy_alphabet();
+        close_files(input, output);
+        return -1;
     }
 
     // Leemos el mensaje a cifrar/descifrar
-    fgets(message, 100, input); // TODO
-    message[strlen(message)-1] = 0;
+    while (fgets(message, keyLength + 1, input) != NULL) {
+        // Si la entrada es stdin en el salto de línea dejamos de leer
+        endRead = (input == stdin) && (strchr(message, '\n') != NULL);
+        message[strcspn(message, "\n")] = '\0';
 
-    if (modo == 0) {
-        // Ciframos el mensaje
-        if (vigenere_encrypt() == -1) {
-            printf("Error: No se ha podido cifrar el mensaje %s con la matriz:\n", message);
+        if (modo == 0) {
+            // Ciframos el mensaje
+            if (vigenere_encrypt(message, k, m) == -1) {
+                printf("Error: No se ha podido cifrar el mensaje %s con la matriz:\n", message);
 
-            destroy_alphabet();
-            close_files(input, output);
-            return -1;
+                free(message);
+                destroy_alphabet();
+                close_files(input, output);
+                return -1;
+            }
+
+            // Mostramos el mensaje cifrado
+            fprintf(output, "%s", message);
+        } else {
+            // Desciframos el mensaje
+            if (vigenere_decrypt(message, k, m) == -1) {
+                printf("Error: No se ha podido descifrar el mensaje %s\n", message);
+
+                free(message);
+                destroy_alphabet();
+                close_files(input, output);
+                return -1;
+            }
+
+            // Mostramos el mensaje descifrado
+            fprintf(output, "%s", message);
         }
 
-        // Mostramos el mensaje cifrado
-        fprintf(output, "El mensaje cifrado es %s\n", message); // TODO
-    } else {
-        // Desciframos el mensaje
-        if (vigenere_decrypt() == -1) {
-            printf("Error: No se ha podido descifrar el mensaje %s\n", message);
+        // Limpiamos el buffer del mensaje
+        memset(message, 0, keyLength + 1);
 
-            destroy_alphabet();
-            close_files(input, output);
-            return -1;
-        }
-
-        // Mostramos el mensaje descifrado
-        fprintf(output, "El mensaje descifrado es %s\n", message); // TODO
+        // Salir del bucle en stdin
+        if (endRead) break;
     }
+
+    if (output == stdout) fprintf(output, "\n");
+
+    // Liberamos memoria
+    free(message);
 
     // Liberamos alfabeto
     destroy_alphabet();
