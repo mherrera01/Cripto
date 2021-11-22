63a2245307747d6325560bab12d58a6ce551fb2c
mherrera01 <miguelherrera8600@gmail.com>
2021-11-22 22:38:49 +0100

    AES Sboxes done
    Co-authored-by: Jose Manuel Freire Porras <jose.freire@estudiante.uam.es>

--- Práctica 2/src/AESSboxesLinearity.c
+++ Práctica 2/src/AESSboxesLinearity.c
@@ -4,40 +4,20 @@
 #include "../includes/AES_tables.h"
 #include "../includes/bits.h"
 
-get_hex_char_value(char hexChar){
-    int value;
-
-    if(hexChar <= )
+int get_hex_char_value(const char* hexChar){
+    return strtol(hexChar, NULL, 16);
 }
-
-//set_byte_to_hex() en bits.c
-
-/*
-for (i = 0; i < strlen(k); i++) {
-    // Convertimos la clave en ASCII a valores hexadecimales
-    strncpy(hexChar, &k[i], 1);
-    hexValue = (char) strtol(hexChar, NULL, 16);
-
-    if (set_byte_to_value(key, i, hexValue) == -1) {
-        printf("Error: La clave necesaria para descifrar no se pudo generar.\n");
-
-        free_mem(key, ivBits, registerEntry, block, message);
-        close_files(input, output);
-        return -1;
-
-    }
-}*/
 
 int getOuterValue(Bits* bits){
     int value = 0;
 
     if(!bits) return 0;
-    
+
     if(get_bit(bits, 0) == '1') value += 8;
     if(get_bit(bits, 1) == '1') value += 4;
-    if(get_bit(bits, 6) == '1') value += 2;
-    if(get_bit(bits, 7) == '1') value += 1;
-
+    if(get_bit(bits, 2) == '1') value += 2;
+    if(get_bit(bits, 3) == '1') value += 1;
+    
     return value;
 }
 
@@ -46,16 +26,16 @@ int getInnerValue(Bits* bits){
 
     if(!bits) return 0;
     
-    if(get_bit(bits, 2) == '1') value += 8;
-    if(get_bit(bits, 3) == '1') value += 4;
-    if(get_bit(bits, 4) == '1') value += 2;
-    if(get_bit(bits, 5) == '1') value += 1;
+    if(get_bit(bits, 4) == '1') value += 8;
+    if(get_bit(bits, 5) == '1') value += 4;
+    if(get_bit(bits, 6) == '1') value += 2;
+    if(get_bit(bits, 7) == '1') value += 1;
 
     return value;
 }
 
 int main(){
-    int i, j, k, linearTimes[NUM_S_BOXES];
+    int i, j, linearTimes = 0;
     Bits *x=NULL, *y=NULL, *z=NULL, *sx=NULL, *sy=NULL, *sz=NULL, *sxor=NULL;
 
     x = init_bits(8);
@@ -65,29 +45,27 @@ int main(){
     sy = init_bits(8);
     sz = init_bits(8);
 
-    for(i = 0; i < NUM_S_BOXES; i++) linearTimes[i] = 0;
-
     for(i = 0; i < 256; i++){
         set_bits_decimal_value(x, i);
         for(j = 0; j < 256; j++){
             set_bits_decimal_value(y, j);
 
-            se    (sx, S_BOXES[getOuterValue(x)][getInnerValue(x)]);
-            set_bits_decimal_value(sy, S_BOXES[getOuterValue(y)][getInnerValue(y)]);
+            set_bits_decimal_value(sx, get_hex_char_value(DIRECT_SBOX[getOuterValue(x)][getInnerValue(x)]));
+            set_bits_decimal_value(sy, get_hex_char_value(DIRECT_SBOX[getOuterValue(y)][getInnerValue(y)]));
             
             z = xor_bits(x, y);
-            set_bits_decimal_value(sz, S_BOXES[k][getOuterValue(z)][getInnerValue(z)]);
+            set_bits_decimal_value(sz, get_hex_char_value(DIRECT_SBOX[getOuterValue(z)][getInnerValue(z)]));
 
             sxor = xor_bits(sx, sy);
 
-            if(get_bits_decimal_value(sxor) == get_bits_decimal_value(sz)) linearTimes[k]++;
+            if(get_bits_decimal_value(sxor) == get_bits_decimal_value(sz)) linearTimes++;
 
             destroy_bits(z);
             destroy_bits(sxor);
         }
     }
 
-    for(i = 0; i < NUM_S_BOXES; i++) printf("La %d S-box tiene un procentaje de no linealidad de: %lf%%\n", i, 100.0 - ((double)linearTimes[i]*100.0)/(64*64));
+    printf("La S-box tiene un procentaje de no linealidad de: %lf%%\n", 100.0 - ((double)linearTimes*100.0)/(256*256));
 
     destroy_bits(x);
     destroy_bits(y);
